a89f9d12c03ad652939ad491d52c0248739ba3f8
j3nnn1 <jcmm986@gmail.com>
2019-09-29 19:13:07 +0000

    Auto-generated commit

--- src/app/cat/interfaces/cat.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface CatInterface {
-    breeds: [];
-    height: number;
-    id: string;
-    url: string;
-    width: number;
-}
--- src/app/cat/interfaces/voting.interface.ts
+++
@@ -1,5 +0,0 @@
-export interface Voting {
-  image_id: string;
-  value: number;
-  sub_id: string;
-}
